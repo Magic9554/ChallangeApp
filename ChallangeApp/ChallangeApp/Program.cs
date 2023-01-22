28cc5806acda395d51efcdb6b1a3b2d0829503dd
slawomir.pienio@o2.pl <slawomir.pienio@o2.pl>
2023-01-22 14:16:04 +0000

    Lekcja nr 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -13,3 +13,57 @@ string resoult3 = name3;
 Console.WriteLine(resoult);
 Console.WriteLine(resoult2); 
 Console.WriteLine(resoult3);
+
+string name4 = "Ewa";
+string sex = "kobieta";
+int age4 = 27;
+
+if (sex == "kobieta" && age4 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age4 == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (sex == "mężczyzna")
+{
+    if (age4 < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
+
+string name5 = "Ewa";
+string sex5 = "kobieta";
+int age5 = 33;
+
+if (name5 == "Ewa" && age5 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else 
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+string name6 = "Ewa";
+string sex6 = "kobieta";
+string sex7 = "Mezczyzna";
+int age6 = 33;
+
+if (name6 == "Ewa" && age6 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name6 == "Ewa" && sex6 == "Mezczyzna")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else 
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
