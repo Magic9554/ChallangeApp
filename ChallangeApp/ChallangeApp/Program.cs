fed10e2c81304471eb808774abb8ff4d021ab5e2
slawomir.pienio@o2.pl <slawomir.pienio@o2.pl>
2023-01-22 13:21:35 +0000

    Lekcja nr 3

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, Slawomir!");
+﻿// "Kobieta ponizej 30 lat"
+// "Ewa, lat 33"
+// "Niepelnoletni Mezczyzna"
+
+string name = "Kobieta ponizej";
+string name2 = "Ewa,";
+string age = " 30 lat";
+string age2 = " lat 33";
+string name3 = "Niepelnoletni Mezczyzna";
+string resoult = name + age;
+string resoult2 = name2 + age2;
+string resoult3 = name3;
+Console.WriteLine(resoult);
+Console.WriteLine(resoult2); 
+Console.WriteLine(resoult3);
